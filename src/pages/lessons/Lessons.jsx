f7c6c6250140e1faf9991d10c5098a03ee7a3448
Chen <chen.jiang.1206@gmail.com>
2023-06-22 18:41:55 -0400

    add sort/search function to lessons;

--- src/pages/lessons/Lessons.jsx
+++ src/pages/lessons/Lessons.jsx
@@ -1,17 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import { Button, TextField } from "@mui/material";
 import Container from "@mui/material/Container";
 import { LessonCard } from "../../components";
 import { useDispatch, useSelector } from "react-redux";
 import { getTeacherLessons, getStudentLessons } from "../../redux/lessonSlice";
+import { sortByDateAsc, sortByDateDesc } from "../../utilities";
 
 const Lessons = () => {
   const dispatch = useDispatch()
   const { user, role } = useSelector((store) => store.user);
   const { myLessons } = useSelector((store) => store.lesson);
+  const [sortedLessons, setSortedLessons] = useState(myLessons);
+  const [searchKeyword, setSearchKeyword] = useState("");
+
 
   useEffect(() => {
+    console.log(myLessons)
     if (role === 'teacher') {
     dispatch(getTeacherLessons(user.id));
     } else if (role === 'student') {
@@ -19,12 +25,48 @@ const Lessons = () => {
     }
   }, []);
 
-  console.log(myLessons)
+
+  const handleSearchChange = (e) => {
+    setSearchKeyword(e.target.value);
+  };
+
+  const filteredLessons = sortedLessons.filter((lesson) => {
+    const { title, description, teacher: {first_name, last_name} } = lesson;
+    const keyword = searchKeyword.toLowerCase();
+    return (
+      title.toLowerCase().includes(keyword) ||
+      description.toLowerCase().includes(keyword) ||
+      `${first_name} ${last_name}`.toLowerCase().includes(keyword)
+    );
+  });
 
   return (
     <Container sx={{ py: 8 }} maxWidth="md">
+      <Button
+        variant="contained"
+        onClick={() => setSortedLessons(sortByDateAsc(myLessons, "start"))}
+      >
+        Sort Asc
+      </Button>
+      <Button
+        variant="contained"
+        onClick={() => setSortedLessons(sortByDateDesc(myLessons, "start"))}
+      >
+        Sort Desc
+      </Button>
+      <Button variant="contained" onClick={() => setSortedLessons(myLessons)}>
+        Unsort
+      </Button>
+
+      <TextField
+        label="Search"
+        variant="outlined"
+        value={searchKeyword}
+        onChange={handleSearchChange}
+      />
+
       <Grid container spacing={4}>
-        {myLessons && myLessons.map((lesson) => (
+        {filteredLessons.map((lesson) => (
           <LessonCard lesson={lesson} key={lesson.id} />
         ))}
       </Grid>
